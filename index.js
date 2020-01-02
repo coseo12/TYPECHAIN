4ebb03aa1d672e7789b044478698df809014ddf2
coseo12 <momo_12@naver.com>
2020-01-02 20:20:22 +0900

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const uname = 'sco', age = 35, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(uname, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,5 @@
+const sayHi = (name: string, age: number, gender: string): void => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi('seo', 35, 'male');
