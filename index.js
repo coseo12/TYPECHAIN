f95b59d6f29e2d7bd2742e66f66718b1f5a7a463
coseo12 <momo_12@naver.com>
2020-01-03 16:49:58 +0900

    modify

--- index.js
+++
@@ -1,6 +0,0 @@
-const uname = 'sco', age = 35, gender = 'male';
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(uname, age, gender);
-//# sourceMappingURL=index.js.map
